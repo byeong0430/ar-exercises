91cb6625b795f1ec76e6f29b3c64a51f50d9b893
byeong0430 <byeong.kim0430@gmail.com>
2018-07-17 23:35:19 +0000

    completed exercises_2

--- exercises/exercise_2.rb
+++ exercises/exercise_2.rb
@@ -1,7 +1,17 @@
 require_relative '../setup'
 require_relative './exercise_1'
 
-puts "Exercise 2"
-puts "----------"
+puts 'Exercise 2'
+puts '----------'
 
 # Your code goes here ...
+# create class called Store and inherit Base method from ActiveRecord
+class Store < ActiveRecord::Base
+end
+
+# find row with id = 1 and 2. save the results as @store1 and @storer2 (instance variables)
+@store1 = Store.find(1)
+@store2 = Store.find(2)
+
+# change name to Toronto and update the change in tb
+@store1.update(name: 'Toronto')
